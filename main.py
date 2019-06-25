00a4cf10b60a76a396da524101027860e6f1ddab
root <ict@cic.ac>
2019-06-25 13:05:58 +0000

    local

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+# This is used when running locally only.
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
